f192bc862d385041354ec03a8dfe58584b393029
Daniyar <daniyar>
2023-09-14 18:39:00 +0500

    Задача

---
+++ 2Homework(1)/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+void DeleteSecondDigit (int num)
+            {
+               int firstDigit = num/100;
+                int thirdDigit = num%10;
+                int number1 = firstDigit * 10 + thirdDigit;
+                System.Console.WriteLine(number1);
+            }
+            DeleteSecondDigit (number);
